42eb19d0ae8132c6a16cc62353c5bdd27fbb97e9
mecaputy <martina.e.caputy@gmail.com>
2018-07-14 18:40:20 -0400

    first

---
+++ public/app.js
@@ -0,0 +1,17 @@
+$.getJSON("/articles", function(data) {
+    for (var i = 0; i < data.length; i++) {
+        // display the articles on the page
+    }
+})
+
+// on click function when someone clicks a p tag
+// empty the notes from the note section
+// save the id from the p tag
+
+//ajax call for the article
+//add the note information to the page
+
+
+//on click function for the savenote button
+
+// run a POST request to change the note
---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var cheerio = require("cheerio");
+var request = require("request");
+
+var db = require("./models");
+
+var PORT = 3000;
+
+var app = express();
+
+// app.use(cheerio("dev"));
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.static("public"));
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+// handlebars
+
+// routes
+
+// listen on port
